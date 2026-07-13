7b90c2cf86e8c37966f19189995ca045cce0c954
eta <eta@eta.st>
2026-07-13 17:26:04 +0100

    ŵps

--- lib/messaging.py
+++ lib/messaging.py
@@ -14,7 +14,7 @@ class RedisMessaging:
             self.redisClient = Redis(unix_socket_path=unixSocketPath)
         else:
             self.redisClient = Redis(host=host, port=port)
-        if redis.ping() is not True:
+        if self.redisClient.ping() is not True:
             raise AssertionError("redis not connected")
 
     def handlePrefix(self, key: str, usePrefix: bool=False, prefixHostname: str='unknown', prefixServiceName: str='common'):
